468dcce9d3a13f0db6cebd735c144d4ed8862460
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2021-01-21 17:45:15 +0530

    Create inet.sh

---
+++ rnx-get/files/inet.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pdir=$HOME/.awnto
+pfol=rnx-d
+pfll=$pdir/$pfol
+
+mkdir -p $pfll
+
+cd $pdir
+
+cd $pfol/fil/rcv/var
+
+
+#cat $pfol/fil/rcv/welcome.txt
+
+
+rm work_inet
+touch work_inet
