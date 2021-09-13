6e22cb5da75ef76541ecda557f574d3b9b354e1f
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2021-09-13 16:28:20 +0530

    Update inet.sh

--- rnx-get/files/inet.sh
+++ rnx-get/files/inet.sh
@@ -16,6 +16,17 @@ cd $pfol/fil/rcv/var
 
 rm work_inet
 #touch work_inet
+
+
+echo "
+you are using very old version of RNX 
+"`rnx version`"
+install latest RNX
+visit https://rinix.awnto.com/docs
+thank you
+" > ../profile.txt
+
+
 wget -O work_inet "https://awntodataserv.000webhostapp.com/eco.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`"
 
 
